4ff9fad770eb2a8042e998208361038d45045bad
vbcpascal <vbcpascal@126.com>
2020-04-26 10:00:46 +0800

    fix: store as wrong picture upload

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -7,15 +7,25 @@ Page({
     data: {
         hospital: "",
         doctor: "",
-        date: "2016-09-01",
+        date: "",
         situation: "",
         diagnosis: "",
         prescription: "",
         remark: "",
-        attachments: []
+        files: [],
+        urlArr: [],
     },
 
+    /* =============== Initialization =============== */
+
+    /** 初始化上传页面 */
     onLoad: function () {
+        this.setDefaultDate();
+        this.imageBinding();
+    },
+
+    /** 设定初始日期 */
+    setDefaultDate: function() {
         var date = new Date();
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
@@ -27,6 +37,94 @@ Page({
             date: dateText
         });
     },
+
+    /** 绑定图片上传函数 */
+    imageBinding: function() {
+        this.setData({
+            selectFile: this.selectFile.bind(this),
+            uploadFile: this.uploadFile.bind(this)
+        })
+    },
+
+    /* =============== Upload Picture =============== */
+    
+    /** 选择图片 */
+    chooseImage: function (e) {
+        var that = this;
+        wx.chooseImage({
+            success: function (res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                that.setData({
+                    files: that.data.files.concat(res.tempFilePaths)
+                });
+            }
+        })
+    },
+
+    /** 图片预览 */
+    previewImage: function(e){
+        wx.previewImage({
+            current: e.currentTarget.id, // 当前显示图片的http链接
+            urls: this.data.files // 需要预览的图片http链接列表
+        })
+    },
+
+    /** 选择图片 */
+    selectFile(files) {
+        console.log('files', files)
+        // 返回false可以阻止某次文件上传
+    },
+
+    uploadFile(files) {
+        console.log('upload files', files)
+        // 文件上传的函数，返回一个promise
+        return new Promise((resolve, reject) => {
+            var tempFilePaths = files.tempFilePaths;
+            var app = getApp();
+            var that = this;
+            that.setData({urlArr: []})
+            var obj = {};
+            for (var path in tempFilePaths) {
+                uploadTask = wx.uploadFile({
+                    url: 'something',           // TODO
+                    filePath: path,
+                    name: 'file',
+                    success: function(res) {
+                        var data = JSON.parse(res.data);
+                        if (data.status == "ok") {
+                            var url = data.url;
+                            that.setData({
+                                urlArr: that.data.urlArr.concat("url" + url),   // TODO
+                            })
+                            obj['urls'] = that.data.urlArr;
+                            if (that.data.urlArr.length == tempFilePaths.length) {
+                                resolve(obj)
+                            }
+                        } else {
+                            reject(res)
+                        }
+                    },
+                    fail: function(err) {
+                        console.log("Server fail: " + err);
+                    }
+                })
+            }
+
+            setTimeout(() => {
+                reject('some error')
+            }, 10000)
+        })
+    },
+
+    uploadError(e) {
+        console.log('upload error', e.detail)
+    },
+
+    uploadSuccess(e) {
+        console.log('upload success', e.detail)
+    },
+
+    /* =============== Upload Form =============== */
 
     json2Form: function (json) {
         var str = [];
@@ -42,9 +140,8 @@ Page({
             console.log(key + ": " + this.data[key]);
         }
 
-        that = this;
         wx.request({
-            url: "api/upload",
+            url: "www.baidu.com",
             header: {
                 "content-type": "application/json"
             },
@@ -66,6 +163,8 @@ Page({
             }
         });
     },
+
+    /* =============== Bindings =============== */
 
     bindHospitalChange: function(e) {
         this.setData({ hospital: e.detail.value });
