015ab787750f2b9bb1a4f816ccee5ca40ea1d7c2
vbcpascal <vbcpascal@126.com>
2020-04-26 10:32:07 +0800

    store: store before merge upload.js

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -25,21 +25,21 @@ Page({
     },
 
     /** 设定初始日期 */
-    setDefaultDate: function() {
+    setDefaultDate: function () {
         var date = new Date();
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
         var day = date.getDate();
 
         var dateText = [year, month, day].map(formatNumber).join('-');
-        
+
         this.setData({
             date: dateText
         });
     },
 
     /** 绑定图片上传函数 */
-    imageBinding: function() {
+    imageBinding: function () {
         this.setData({
             selectFile: this.selectFile.bind(this),
             uploadFile: this.uploadFile.bind(this)
@@ -47,7 +47,7 @@ Page({
     },
 
     /* =============== Upload Picture =============== */
-    
+
     /** 选择图片 */
     chooseImage: function (e) {
         var that = this;
@@ -62,7 +62,7 @@ Page({
     },
 
     /** 图片预览 */
-    previewImage: function(e){
+    previewImage: function (e) {
         wx.previewImage({
             current: e.currentTarget.id, // 当前显示图片的http链接
             urls: this.data.files // 需要预览的图片http链接列表
@@ -75,49 +75,27 @@ Page({
         // 返回false可以阻止某次文件上传
     },
 
+    /** 
+     * 上传图片：仅为本地选择，并不上传到服务器，需要额外处理。方法可以参照4ff9fa
+     * （fix: store as wrong picture upload）提交内容。 
+     */
     uploadFile(files) {
         console.log('upload files', files)
         // 文件上传的函数，返回一个promise
         return new Promise((resolve, reject) => {
-            var tempFilePaths = files.tempFilePaths;
-            var app = getApp();
-            var that = this;
-            that.setData({urlArr: []})
-            var obj = {};
-            for (var path in tempFilePaths) {
-                uploadTask = wx.uploadFile({
-                    url: 'something',           // TODO
-                    filePath: path,
-                    name: 'file',
-                    success: function(res) {
-                        var data = JSON.parse(res.data);
-                        if (data.status == "ok") {
-                            var url = data.url;
-                            that.setData({
-                                urlArr: that.data.urlArr.concat("url" + url),   // TODO
-                            })
-                            obj['urls'] = that.data.urlArr;
-                            if (that.data.urlArr.length == tempFilePaths.length) {
-                                resolve(obj)
-                            }
-                        } else {
-                            reject(res)
-                        }
-                    },
-                    fail: function(err) {
-                        console.log("Server fail: " + err);
-                    }
-                })
-            }
-
-            setTimeout(() => {
-                reject('some error')
-            }, 10000)
+            resolve({
+                urls: files.tempFilePaths
+            }); // use tmp paths, should get url from the server
         })
     },
 
+    /** 上传失败返回函数 */
     uploadError(e) {
-        console.log('upload error', e.detail)
+        wx.showModal({
+            title: '提示',
+            content: '图片上传失败\n' + e.detail,
+            showCancel: false,
+        })
     },
 
     uploadSuccess(e) {
@@ -166,32 +144,46 @@ Page({
 
     /* =============== Bindings =============== */
 
-    bindHospitalChange: function(e) {
-        this.setData({ hospital: e.detail.value });
+    bindHospitalChange: function (e) {
+        this.setData({
+            hospital: e.detail.value
+        });
     },
 
-    bindDoctorChange: function(e) {
-        this.setData({ doctor: e.detail.value });
+    bindDoctorChange: function (e) {
+        this.setData({
+            doctor: e.detail.value
+        });
     },
 
-    bindDateChange: function(e) {
-        this.setData({ date: e.detail.value });
+    bindDateChange: function (e) {
+        this.setData({
+            date: e.detail.value
+        });
     },
 
-    bindSituationChange: function(e) {
-        this.setData({ situation: e.detail.value });
+    bindSituationChange: function (e) {
+        this.setData({
+            situation: e.detail.value
+        });
     },
 
-    bindDiagnosisChange: function(e) {
-        this.setData({ diagnosis: e.detail.value });
+    bindDiagnosisChange: function (e) {
+        this.setData({
+            diagnosis: e.detail.value
+        });
     },
 
-    bindPrescriptionChange: function(e) {
-        this.setData({ prescription: e.detail.value });
+    bindPrescriptionChange: function (e) {
+        this.setData({
+            prescription: e.detail.value
+        });
     },
 
-    bindRemarkChange: function(e) {
-        this.setData({ remark: e.detail.value });
+    bindRemarkChange: function (e) {
+        this.setData({
+            remark: e.detail.value
+        });
     },
 
 });
