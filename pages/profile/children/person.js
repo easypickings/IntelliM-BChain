23958499d9b5a5d87b3f84e3ab6c14f5fed711ce
kable <liukaibo0223@gmail.com>
2020-05-23 19:44:17 +0800

    fix:personInfo page logic fix

--- pages/profile/children/person.js
+++ pages/profile/children/person.js
@@ -33,18 +33,48 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: async function () {
-    console.log("tring set baseInfo:")
+    console.log("onLoad:")
     if(app.globalData.baseInfo){
       this.setData({baseInfo: app.globalData.baseInfo});
     }
     else{
-        console.log('baseInfo downLoad fail:No globalData');
-      
+        console.log('baseInfo downLoad fail:No globalData'); 
+    }
+  },
+
+  onShow: async function () {
+    console.log("onShow:")
+    if(app.globalData.baseInfo){
+      this.setData({baseInfo: app.globalData.baseInfo});
+    }
+    else{
+      if (app.globalData.token) {
+        try {
+          await this.getBaseInfo();
+          this.setData({baseInfo: app.globalData.baseInfo});
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        console.log('profile onShow fail:No token');
+      }
     }
   },
 
 
   /** 下载基础数据 */
+  getBaseInfo: async function () {
+    let that = this;
+    try {
+      console.log("trying download baseInfo")
+      let res = await server.getBaseInfo(app.globalData.token);
+      app.globalData.baseInfo = res;
+    } catch (e) {
+      console.log(e);
+      utils.showToast('基础信息查询失败');
+    }
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -52,12 +82,6 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面隐藏
